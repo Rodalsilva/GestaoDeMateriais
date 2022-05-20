add5e515e8559df669afae8cf8bc5fbd63ac1103
Rodrigo Alves <r.alves2505@hotmail.com>
2022-05-19 23:55:18 -0300

    Criando um programa de gestão de materiais

---
+++ GerenciaDeMateriais/Produto.cs
@@ -0,0 +1,15 @@
+﻿namespace GerenciaDeMateriais
+{
+    internal class Produto
+    {
+        public string Nome;
+        public double Preco;
+        public int Quantidade;
+
+        public double ValorTotalEmEstoque()
+        {
+            return Preco * Quantidade;
+        }
+
+    }
+}
